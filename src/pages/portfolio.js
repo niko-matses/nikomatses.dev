9cb45dfd080ddcd31d658e2e24aefb56c29f9183
niko-matses <nikolas.matses@gmail.com>
2020-01-02 16:43:30 -0500

    passed location props to generate static build without errors

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,8 +2,8 @@ import React from 'React';
 import Layout from '../components/layout';
 import Projects from '../components/projects';
 
-const Portfolio = () => (
-  <Layout>
+const Portfolio = ({ location }) => (
+  <Layout location={location}>
     <section className="section__about">
       <h1 className="section__heading">Projects</h1>
       <p className="section__intro">Here are some web apps I've worked on.</p>
