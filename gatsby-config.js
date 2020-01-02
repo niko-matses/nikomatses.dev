2296c244f1c22ae6ef32c55f97dc0a89b9693656
niko-matses <nikolas.matses@gmail.com>
2020-01-02 17:20:41 -0500

    added config.yml for netlify cms integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
     },
 
     'gatsby-transformer-remark',
-    'gatsby-plugin-netlify',
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-netlify', //Make sure to put this last
   ],
 };
